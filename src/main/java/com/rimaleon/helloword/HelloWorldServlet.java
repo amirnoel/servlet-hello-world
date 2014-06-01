d444b47e40416b12498ee3399743816c8ab46691
Amir Noel <amir@rimaleon.com>
2014-06-01 14:43:51 -0400

    Add index.jsp files

--- src/main/java/com/rimaleon/helloword/HelloWorldServlet.java
+++ src/main/java/com/rimaleon/helloword/HelloWorldServlet.java
@@ -14,7 +14,7 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)  
             throws ServletException, IOException {
-        resp.getOutputStream().write("Hello World.".getBytes());
+        resp.getOutputStream().write("Hello World Servlet.".getBytes());
     }
 }
 
